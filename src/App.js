d7bd6c4b4f31601d2943a4e02acd6bc510c60f0a
Matt Cooper <matisskomlevs@gmail.com>
2023-01-12 09:36:40 +0200

    mui implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import * as React from 'react';
 import './App.css';
 import Box from './components/Box'
 
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+
 function App() {
   return (
-    <>
+    <body>
       <h2>Nothing for now</h2>
       <Box />
-    </>
+      <Button 
+        variant="outlined"
+        size="small"
+
+        >
+          boop
+      </Button>
+    </body>
   );
 }
+
 
 export default App;
